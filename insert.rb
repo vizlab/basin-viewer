ef8804ab1ece0cce2673a6f2d067144801adbd75
Takao NAITO <xarsh@outlook.com>
2017-06-28 15:29:17 +0900

    Add insert script.

--- insert.rb
+++ insert.rb
@@ -1,8 +1,23 @@
 require 'csv'
+require 'json'
 require 'mongo'
 
-client = Mongo::Client.new('mongodb://localhost:27017/basin')
+client = Mongo::Client.new('mongodb://localhost:27017/basin3')
 collection = client[:record]
+
+records = []
+JSON.parse(File.read('./japan-basin.geojson'))['features'].each do |f|
+  att = f['properties']
+  records.push({
+    mesh_code: att['W07_001'].to_i,
+    valley_code: att['W07_002'].to_i,
+    river_code: att['W07_003'].to_i,
+    valley_name: att['W07_004'],
+    river_name: att['W07_005'],
+    loc: f['geometry']
+  })
+end
+collection.insert_many(records)
 
 i = 0
 CSV.foreach('./rain_2050_09.csv', headers: true) do |row|
