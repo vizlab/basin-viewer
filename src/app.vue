7e4327d98c83d77cae0ee875c445e42b3ca8a891
Yosuke ONOUE <onoue@likr-lab.com>
2017-10-20 20:03:45 +0900

    Change chart components interface

--- src/app.vue
+++ src/app.vue
@@ -28,6 +28,29 @@
 <script>
 import Datepicker from 'vuejs-datepicker';
 
+const convertRainfallData = rainfall => {
+  const labels = Object.keys(rainfall).map(r => {
+    const d = new Date(`${r.slice(8,12)}/${r.slice(5,8)}/${r.slice(3,5)} ${r.slice(0,2)}:00`);
+    d.setTime(d.getTime() - d.getTimezoneOffset()*60*1000);
+    return d.toISOString().replace(/-/g, '/').replace('T', ' ').slice(0, 16);
+  });
+  const data = Object.keys(rainfall).sort((a, b) => {
+    const a2 = a.slice(0,5).split('Z').reverse().join('');
+    const b2 = b.slice(0,5).split('Z').reverse().join('');
+    return a2 > b2 ? -1 : 1;
+  }).map(key => rainfall[key]);
+  return {
+    labels,
+    ensembles: [
+      {name: 'Ensemble 1', data},
+      {name: 'Ensemble 2', data: data.map(n => n * Math.random())},
+      {name: 'Ensemble 3', data: data.map(n => n * Math.random() * 2)},
+      {name: 'Ensemble 4', data: data.map(n => n * Math.random() * 4)},
+      {name: 'Ensemble 5', data: data.map(n => n * Math.random() * 8)},
+    ]
+  };
+};
+
 export default {
   components: {Datepicker},
   data: () => ({
@@ -49,7 +72,7 @@ export default {
       .then(res => {
         if(res.length === 0) return;
         this.basins = res.basins;
-        this.rainfall = res.rainfall;
+        this.rainfall = convertRainfallData(res.rainfall);
       });
     },
     swapLatLng(polygon) {
--- src/charts/basic-line-chart.vue
+++ src/charts/basic-line-chart.vue
@@ -5,20 +5,14 @@ highcharts(:options="chartOptions")
 export default {
   props: ['rainfall'],
   data: () => ({
-    chartOptions: {}
+    chartOptions: {
+      title: {
+        text: null
+      }
+    }
   }),
   watch: {
-    rainfall (rainfall) {
-      const labels = Object.keys(rainfall).map(r => {
-        const d = new Date(`${r.slice(8,12)}/${r.slice(5,8)}/${r.slice(3,5)} ${r.slice(0,2)}:00`);
-        d.setTime(d.getTime() - d.getTimezoneOffset()*60*1000);
-        return d.toISOString().replace(/-/g, '/').replace('T', ' ').slice(0, 16);
-      });
-      const data = Object.keys(rainfall).sort((a, b) => {
-        const a2 = a.slice(0,5).split('Z').reverse().join('');
-        const b2 = b.slice(0,5).split('Z').reverse().join('');
-        return a2 > b2 ? -1 : 1;
-      }).map(key => rainfall[key]);
+    rainfall ({ensembles, labels}) {
       this.chartOptions = {
         title: {
           text: null
@@ -35,13 +29,7 @@ export default {
             text: 'Rainfall'
           }
         },
-        series: [
-          {name: 'Ensemble 1', data},
-          {name: 'Ensemble 2', data: data.map(n => n * Math.random())},
-          {name: 'Ensemble 3', data: data.map(n => n * Math.random() * 2)},
-          {name: 'Ensemble 4', data: data.map(n => n * Math.random() * 4)},
-          {name: 'Ensemble 5', data: data.map(n => n * Math.random() * 8)},
-        ]
+        series: ensembles
       };
     }
   }
