46b9fb174b8fda3642f27c1912558384357574e1
Yosuke ONOUE <onoue@likr-lab.com>
2018-04-07 14:40:41 +0900

    remove old files

--- insert.rb
+++
@@ -1,9 +0,0 @@
-require 'json'
-require 'mongo'
-
-Mongo::Logger.logger.level = ::Logger::FATAL
-DB = Mongo::Client.new('mongodb://127.0.0.1:27017/sicat')[:basin]
-DB.drop
-docs = JSON.parse(File.read('./data/japan-basin.geojson'))['features'].delete_if{|f| f['geometry']['coordinates'].empty?}
-DB.insert_many(docs)
-DB.indexes.create_one('geometry' => '2dsphere')
